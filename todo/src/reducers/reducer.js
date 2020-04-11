d31e50f830ee3731a41f216c45fb0b772b0d6e8d
Daniel Hermansen <daniel.j.hermansen@gmail.com>
2020-04-11 10:20:26 -0600

    finished all components and reducers

---
+++ todo/src/reducers/reducer.js
@@ -0,0 +1,32 @@
+export const todo = [
+    {
+        item: "Clean Bathroom",
+        completed: false,
+        id: 6345234543
+    }
+];
+
+export const reducer = (state, action) => {
+    console.log("state", state, "action", action);
+    switch (action.type) {
+        case "ADD_TODO":
+            console.log("case state", state);
+            return [
+                ...state,
+                { item: action.payload, completed: false, id: Date.now() }
+            ];
+
+        case "TOGGLE_COMPLETED": 
+            return state.map(todo => {
+                if (todo.id === action.payload) {
+                    return { ...todo, completed: !todo.completed };
+                }
+                return todo;
+            });
+
+        case "CLEAR_COMPLETED":
+            return state.filter(todo => !todo.completed);
+        default:
+            return state;
+    }
+};
